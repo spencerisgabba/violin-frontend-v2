f9ea1171031feb15fba258a5ce5abe7fc898c2a2
Spencer Schmidt <spennccee.schmidt0@gmail.com>
2024-10-27 22:04:46 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+    env:{
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+    },
     images: {
         remotePatterns: [
             {
