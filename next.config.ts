989a5f5cc251ddb66f36b4a7ae6e0888fe05501a
Spencer Schmidt <spennccee.schmidt0@gmail.com>
2024-10-28 00:00:47 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 import './envConfig.ts'
 const nextConfig: NextConfig = {
+    output: 'standalone',
     env: {
         NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
         NEXT_PUBLIC_ANALYTICS_ID: process.env.NEXT_PUBLIC_ANALYTICS_ID,
