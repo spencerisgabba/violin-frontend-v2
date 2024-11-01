993ccee984fff0332ee82b655b7ae843f75e8ec0
Spencer Schmidt <spennccee.schmidt0@gmail.com>
2024-11-01 09:11:36 -0500

    Update VideoComponent.tsx

--- app/components/VideoComponent.tsx
+++ app/components/VideoComponent.tsx
@@ -4,7 +4,7 @@ const VideoComponent = () => {
     return (
         <div className="video-container">
             <video className="video-element" autoPlay loop muted>
-                <source src="/videos/train.webm" type="video/webm" />
+                <source src="/videos/train.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
         </div>
