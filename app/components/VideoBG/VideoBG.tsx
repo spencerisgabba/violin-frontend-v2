d9fad3d0dff7249742edc2c0cd697daadb50b7b6
Spencer Schmidt <spennccee.schmidt0@gmail.com>
2024-11-20 13:24:21 -0600

    Update VideoBG.tsx

--- app/components/VideoBG/VideoBG.tsx
+++ app/components/VideoBG/VideoBG.tsx
@@ -1,11 +1,13 @@
 export const VideoBG = () => {
-    return (
-            <div className="homeimg">
-                <video preload="auto" controls={false} playsInline autoPlay muted loop>
-                    <source src="https://videos.pexels.com/video-files/5652224/5652224-uhd_3840_2160_24fps.mp4"
-                            type="video/mp4"/>
-                    Your browser does not support the video tag.
-                </video>
-            </div>
-    );
+  return (
+    <div className="homeimg">
+      <video preload="true" controls={false} playsInline autoPlay muted loop>
+        <source
+          src="https://res.cloudinary.com/dztnyrx5z/video/upload/v1732058868/IMG_0555-vmake_sazm5v.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 };
